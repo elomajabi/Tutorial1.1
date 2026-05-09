64674af1f8f909fe4252b2783424e2d34d7c7af5
maja <bilmaj007@wp.pl>
2026-05-09 11:45:58 +0200

    Take a list of VIP guest names and print successs if the name is in the list

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print("first message")
+# Task 1 - Make a program that asks for names. If the name is on our VIP list, they get in. If not, they are rejected. We will use a loop so the program keeps running until we type “exit”
+
+vip_names = ["Wojtek" , "Norbert" , "Kalina" , "Maja"]
+
+while True:
+    name = input("What is your name? ")
+
+    if name.lower() == "exit":
+        print("Goodbye!")
+        break
+
+    if name in vip_names:
+        print(f"Welcome to VIP, {name}")
+    else:
+        print("Acces denied")
